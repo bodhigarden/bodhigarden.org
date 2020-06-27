adfac0025653e8ae85b3c51946fac5bb4de0e77f
Chris Dornan <chris@chrisdornan.com>
2020-06-27 20:13:47 +0100

    index.php

---
+++ docs/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bodhi Garden - Welcome to the Bodhi Garden</title>
+        <link rel="stylesheet" href="./assets/scss/site.css" />
+        <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
+    </head>
+    <body>
+        <header class="uk-section-secondary" uk-sticky>
+
+    <nav class="uk-navbar" uk-navbar style="display: flex; justify-content: space-between;">
+        <ul class="uk-navbar-nav">
+            <li><a href="./"><span uk-icon="home"></span>&nbsp;The Bodhi Garden</a></li>
+        </ul>
+        <div class="uk-navbar-flip">
+            <ul class="uk-navbar-nav">
+                <li><a href="./pages/dana.html"><span uk-icon="info"></span>&nbsp;Donations</a></li>
+                <li><a href="./pages/links.html"><span uk-icon="info"></span>&nbsp;Links</a></li>
+                <li><a href="./pages/about.html"><span uk-icon="info"></span>&nbsp;Bodhi Garden</a></li>
+            </ul>
+        </div>
+    </nav>
+
+</header>
+<div class="uk-margin"></div>
+
+
+        <main uk-height-viewport="expand: true">
+          <div class="uk-container cd-article-container cd-running-font">
+            <article class="uk-article">
+    <h1 class="uk-article-title">Welcome to the Bodhi Garden</h1>
+    <p class="uk-article-meta">
+        
+    </p>
+    
+    <p>Welcome to the Bodhi Garden, a non-denominational Buddhist charity based in the UK.</p>
+<p>Bodhi Garden means, literally, ‘garden of awakening’. It is for anyone drawn to Buddhist teachings, meditation practice, and dharma enquiry. It operates on the traditional Buddhist principle of <i><a href="./pages/dana.html">dana</a></i>.</p>
+<p>The Bodhi Garden was established in 2001 by the English Buddhist Monastery Trust, a UK-registered charity (no. 1088804), whose object is the support the study and practice of the Buddhist way of life, and whose ethos is one of non-sectarianism.</p>
+<p>If you would like to join our email list, please contact us at <a href="mailto:info@bodhigarden.org">info@bodhigarden.org</a>.</p>
+<p><span uk-icon="facebook"></span> <a href="https://en-gb.facebook.com/pages/category/Nonprofit-Organization/Bodhi-Garden-122324451175547/">Bodhi Garden</a></p>
+<div class="uk-margin-small-top"></div>
+</article>
+
+          </div>
+        </main>
+
+        <footer class="cd-footer uk-section uk-section-secondary">
+          <div class="uk-container">
+              <p style="text-align:center;"><span uk-icon="mail"></span> <a href="mailto:info@bodhigarden.org">info@bodhigarden.org</a>
+                 | <span uk-icon="facebook"></span> <a href="https://en-gb.facebook.com/pages/category/Nonprofit-Organization/Bodhi-Garden-122324451175547/">Bodhi Garden</a>
+              </p>
+          </div>
+        </footer>
+
+        <script src="https://getuikit.com/assets/uikit/dist/js/uikit.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.3.7/js/uikit-icons.min.js"></script>
+    </body>
+</html>
